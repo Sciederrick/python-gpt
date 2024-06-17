d34bf17c7f6ca462b253838dca0cbd736139e1bc
Sciederrick <derrickmbarani@gmail.com>
2024-06-17 22:08:09 +0300

    feat: icon library, logo and color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,25 @@ export default {
         "./error.vue",
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primary: {
+                    light: "#4285F4", // Lighter blue, similar to Twitter's primary
+                    DEFAULT: "#007BFF", // Standard blue, similar to Bootstrap's primary
+                    dark: "#005CBE", // Darker blue
+                },
+                secondary: {
+                    light: "#F8F9FA", // Light gray for backgrounds
+                    DEFAULT: "#868E96", // Medium gray for text
+                    dark: "#343A40", // Dark gray for accents
+                },
+                accent: {
+                    light: "#F9F7F6", // Very light gray for highlights
+                    DEFAULT: "#FFC107", // Orange for emphasis
+                    dark: "#E0A800", // Darker orange
+                },
+            },
+        },
     },
     plugins: [],
 };
